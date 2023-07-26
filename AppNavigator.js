487ee51ba1bbe91c079cdaa6f56064334f4bf0bd
Sisi Small <smallsianah@gmail.com>
2023-07-26 08:24:22 -0500

    'Add New Recipe' and 'View Calendar' buttons added

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Button } from 'react-native';
 import HomeScreen from './HomeScreen';
 import AddRecipeScreen from './AddRecipeScreen';
 import CalendarScreen from './CalendarScreen';
@@ -9,7 +10,25 @@ const Stack = createStackNavigator();
 const AppNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen 
+        name="Home" 
+        component={HomeScreen}
+        options={({ navigation }) => ({
+          title: 'Home',
+          headerRight: () => (
+            <Button 
+              title="Add New Recipe" 
+              onPress={() => navigation.navigate('AddRecipe')} 
+            />
+          ),
+          headerLeft: () => (
+            <Button 
+              title="View Calendar" 
+              onPress={() => navigation.navigate('Calendar')} 
+            />
+          )
+        })}
+      />
       <Stack.Screen name="AddRecipe" component={AddRecipeScreen} />
       <Stack.Screen name="Calendar" component={CalendarScreen} />
     </Stack.Navigator>
@@ -17,3 +36,4 @@ const AppNavigator = () => {
 };
 
 export default AppNavigator;
+
