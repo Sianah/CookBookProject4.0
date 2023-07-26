850534de2b29c8c2980f4344315be488bb23c19e
Sisi Small <smallsianah@gmail.com>
2023-07-26 11:56:15 -0500

    Added feature where inputs are saved and loaded

--- RecipeContext.js
+++ RecipeContext.js
@@ -1,9 +1,39 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const RecipeContext = createContext();
 
 export const RecipeProvider = ({ children }) => {
     const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        // Load recipes when the app starts
+        const loadRecipes = async () => {
+            try {
+                const savedRecipes = await AsyncStorage.getItem('recipes');
+                if (savedRecipes) {
+                    setRecipes(JSON.parse(savedRecipes));
+                }
+            } catch (error) {
+                console.error("Failed to load the recipes.", error);
+            }
+        };
+
+        loadRecipes();
+    }, []);
+
+    useEffect(() => {
+        // Save recipes whenever they change
+        const saveRecipes = async () => {
+            try {
+                await AsyncStorage.setItem('recipes', JSON.stringify(recipes));
+            } catch (error) {
+                console.error("Failed to save the recipes.", error);
+            }
+        };
+
+        saveRecipes();
+    }, [recipes]);
 
     const addRecipe = (recipe) => {
         setRecipes(prev => [...prev, recipe]);
@@ -29,6 +59,7 @@ export const useRecipes = () => {
     }
     return context;
 };
+
 
 
 
