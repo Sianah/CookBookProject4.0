29173a9a684fb3b6ffe2cb77abb80cd2dce3d956
Sisi Small <smallsianah@gmail.com>
2023-07-26 08:40:41 -0500

    Fixed boxes with delete button added

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ScrollView, Text, View, Button, StyleSheet } from 'react-native';
-import { useRecipes } from './RecipeContext';
+import { useRecipes } from './RecipeContext';  // Ensure path is correct
 
 function HomeScreen({ navigation }) {
   const { recipes, deleteRecipe } = useRecipes();
@@ -10,8 +10,16 @@ function HomeScreen({ navigation }) {
       <Text style={styles.header}>Recipe List</Text>
       {recipes.map((recipe, index) => (
         <View key={index} style={styles.recipeCard}>
-          <Text style={styles.recipeName}>{recipe.name}</Text>
-          <Button title="Delete" onPress={() => deleteRecipe(index)} />
+          <View style={styles.recipeContent}>
+            <Text style={styles.recipeName}>{recipe.name}</Text>
+            <Text style={styles.recipeDetail}>{recipe.details}</Text>
+            {recipe.ingredients.map((ingredient, idx) => (
+              <View key={idx} style={styles.ingredientRow}>
+                <Text>{ingredient.name}: {ingredient.measurement} {ingredient.unit}</Text>
+              </View>
+            ))}
+          </View>
+          <Button title="Delete" color="red" onPress={() => deleteRecipe(index)} style={styles.deleteButton} />
         </View>
       ))}
     </ScrollView>
@@ -35,10 +43,37 @@ const styles = StyleSheet.create({
     padding: 15,
     borderRadius: 8,
     marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+  },
+  recipeContent: {
+    flex: 1,
   },
   recipeName: {
     fontSize: 18,
+    marginBottom: 5,
+  },
+  recipeDetail: {
+    fontSize: 14,
+    marginBottom: 10,
+  },
+  ingredientRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  deleteButton: {
+    marginLeft: 10,
   },
 });
 
 export default HomeScreen;
+
