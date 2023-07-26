25a8fa299abafa08aef37fb8af4d8b85e993e138
Sisi Small <smallsianah@gmail.com>
2023-07-26 08:46:57 -0500

    fixed calendar screen and removed the button

--- CalendarScreen.js
+++ CalendarScreen.js
@@ -1,21 +1,50 @@
-// CalendarScreen.js
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { Calendar, LocaleConfig } from 'react-native-calendars';
 
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+LocaleConfig.locales['en'] = {
+  monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+  monthNamesShort: ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'Jun', 'Jul.', 'Aug', 'Sep.', 'Oct.', 'Nov.', 'Dec.'],
+  dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+  dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+};
 
-export default function CalendarScreen() {
+LocaleConfig.defaultLocale = 'en';
+
+export default function CalendarScreen({ navigation }) {
+  const [markedDates, setMarkedDates] = useState({});
+
+  const onDayPress = (day) => {
+    let newMarkedDates = { ...markedDates };
+
+    // If date is already selected, unselect it
+    if (newMarkedDates[day.dateString]) {
+      delete newMarkedDates[day.dateString];
+    } else {
+      newMarkedDates[day.dateString] = {
+        selected: true,
+        marked: true,
+      };
+    }
+    setMarkedDates(newMarkedDates);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Calendar Screen</Text>
+      <Calendar
+        onDayPress={onDayPress}
+        markedDates={markedDates}
+      />
+    
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#eaeaea',
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#eaeaea',
+    padding: 10,
+  },
 });
+
